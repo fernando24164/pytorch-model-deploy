b7edb944fab492025f36ed374edbf81ffc00d35a
fernando24164 <fernando24164@gmail.com>
2020-05-10 20:57:26 +0200

    Fix model path

--- src/model/training.py
+++ src/model/training.py
@@ -4,22 +4,33 @@ from pathlib import Path
 import torch
 from torch.autograd import Variable
 
-from regression import LinearRegressionModel
+
+class LinearRegressionModel(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear = torch.nn.Linear(1, 1)
+
+    def forward(self, x):
+        y_pred = self.linear(x)
+        return y_pred
+
 
 # Declaring variables
 x_data = Variable(torch.Tensor([[1.0], [2.0], [3.0], [4.0], [5.0]]))
 y_data = Variable(torch.Tensor([[2.0], [4.0], [6.0], [8.0], [10.0]]))
 
+
 def save_model(model):
-    with open(str(Path.cwd()) +'/src/model/model.pkl', 'wb') as file:
+    with open(str(Path.cwd()) + "/src/model/model.pkl", "wb") as file:
         torch.save(model, file)
+
 
 # Create model
 model = LinearRegressionModel()
 
 # Trainning model
-criterion = torch.nn.MSELoss(size_average = False)
-optimizer = torch.optim.SGD(model.parameters(), lr = 0.01)
+criterion = torch.nn.MSELoss(size_average=False)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
 
 for time in range(50):
     pred_y = model(x_data)
@@ -29,7 +40,7 @@ for time in range(50):
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
-    print('Scenario {}, loss {}'.format(time, loss.item()))
+    print("Scenario {}, loss {}".format(time, loss.item()))
 
 
 new_var = Variable(torch.Tensor([[6.0]]))
